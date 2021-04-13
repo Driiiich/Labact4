227b79f6478d34dd3cce6a39308c58f1e9ed5160
Driiiich <82429762+Driiiich@users.noreply.github.com>
2021-04-13 12:22:24 +0800

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,6 @@
+first = (str(input('first name : ')))
+last = (str(input('last name : ')))
+location = (str(input('location : ')))
+age = (str(input('age : ')))
+space =""
+print(first, space, last, space, location, space, age) 
